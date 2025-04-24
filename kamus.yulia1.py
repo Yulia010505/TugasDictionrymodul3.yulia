d89133c6de7bf0dfbd78b79bd07d5f8e55b9069a
Yulia010505 <yuliasuherni369@gmail.com>
2025-04-24 08:31:47 +0800

    Add files via upload

---
+++ kamus.yulia1.py
@@ -0,0 +1,11 @@
+kamus ={"jurusan Informatika":" ilmu yang mempelajari tentang ilmu komputer",
+        "jurusan Farmasi":" ilmu yang mempelajari tentang pengembangan dan penggunaan obat-obatan",
+        "jurusan Peternakan":" ilmu yang mempelajari pengembangan sumber daya hewan",
+        "jurusan Arsitektur":" ilmu yang mempelajari desain dan perencanaan bangunan"
+        }
+kata= input("masukkan kata yang ingin anda cari di dalam kamus:")
+
+if kata in kamus:
+    print(f"Arti dari'{kata}' adalah: {kamus[kata]}")
+else:
+    print(f"sorry,  arti dari'{kata}' yang anda cari tidak terdaftar dalam kamus.")
